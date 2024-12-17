1d43c7104e7bb75c771d95a247f6c83d52eb81a0
vipul279 <vvpvvp872@gmail.com>
2024-12-17 19:11:47 +0530

    Solved responsive issues

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './MovieCard.css';
  
 
 const MovieCard = ({movie}) => {
   return (
-    <div className="movie-card" style={{height:'490px'}}>
+    <div className="movie-card" >
 
         <Link to={`/movie/${movie.imdbID}`}>
             <img src={movie.Poster} alt="" />
